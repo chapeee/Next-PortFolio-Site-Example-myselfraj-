c63f122cbe5d21b6410a09a329593793765f525a
grapdevs <rajneesh@grapdevs.com>
2024-03-08 12:09:15 +0530

    forgot use quot

--- src/app/eg/gist/Examples.tsx
+++ src/app/eg/gist/Examples.tsx
@@ -7,13 +7,13 @@ export default function Examples() {
       <h2 className="text-4xl mb-5">Gist Cod Sample</h2>
       <code>
         <pre>
-          "use client"<br/> 
-          import GitGist from "@myselfraj/react-nextjs-github-gist";<br /><br />
+        &quot;use client&quot;<br/> 
+          import GitGist from &quot;@myselfraj/react-nextjs-github-gist&quot;;<br /><br />
           &lt;GitGist 
-          <br />id="1036aa9bf78709602485f0a9f9e47e50" <br />
-          file=""
-          <br />userName="chapeee" <br />getProfile="true" <br />height="700px"{" "}
-          <br />width="100%" <br />/&gt;
+          <br />id=&quot;1036aa9bf78709602485f0a9f9e47e50&quot; <br />
+          file=&quot;&quot;
+          <br />userName=&quot;chapeee&quot; <br />getProfile=&quot;true&quot; <br />height=&quot;700px&quot;{" "}
+          <br />width=&quot;100%&quot; <br />/&gt;
         </pre>
       </code>
       <div className="mt-[50px]">
