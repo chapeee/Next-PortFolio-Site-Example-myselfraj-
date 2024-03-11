4b16a18b9424dc729edda08f47d533ca57578d9c
grapdevs <rajneesh@grapdevs.com>
2024-03-11 18:54:05 +0530

    solved minor issue for mac

--- src/app/Components/Homepage/DigitalMarvels.tsx
+++ src/app/Components/Homepage/DigitalMarvels.tsx
@@ -11,7 +11,7 @@ export default function DigitalMarvels() {
     <div>
       {" "}
       <div className="z-[999] relative bg-gray-100">
-        <div className="bg-siteDefaultColor text-white pt-14 pb-14 min-h-[100vh] flex justify-center ">
+        <div className="bg-siteDefaultColor text-white pt-14 pb-14 md:min-h-[500px] lg:min-h-[100vh] flex justify-center ">
           <div className="container m-auto p-5 pt-0 pb-0">
             <div
               className="text-[48px] sm:-[58px] lg:text-8xl font-medium text-center "
